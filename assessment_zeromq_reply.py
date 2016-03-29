d98ff15fa5bf57135c31b615815db0db2e8060cb
vanand <vanand@zeomega.com>
2016-03-29 13:37:38 +0530

    ZeroMQ Assessment 1 : a) Requesting and Replying                       b) Publishing and Subscribing(Multiple Times) the local host with ZeroMQ messages.

--- assessment_zeromq_reply.py
+++ assessment_zeromq_reply.py
@@ -1,4 +1,4 @@
-# This is the simple program for ZeroMQ module with Requesting other server.
+# This is the simple program for ZeroMQ module with Replying other server.
 
 # Imports
 import zmq
@@ -8,7 +8,10 @@ socket = context.socket(zmq.REP)
 # Host that will reply to the requesting host.
 socket.bind("tcp://127.0.0.1:5000")
  
+# while True:
+#     msg = socket.recv()
+#     print "Got", msg
+#     socket.send(msg)
+
 while True:
-    msg = socket.recv()
-    print "Got", msg
-    socket.send(msg)
+    print  socket.recv()
