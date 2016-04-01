c85cdd4940d6f2b6584a37b0d67673742b681fdd
vanand <vanand@zeomega.com>
2016-04-01 21:40:50 +0530

    Assessment ETL : Validating the received data from csv and sending them to session.

--- agents/loader_data.py
+++ agents/loader_data.py
@@ -4,10 +4,20 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os, datetime
+import zmq
+import json
 
 # the declarative_base() callable returns a new base class from which all mapped classes should inherit
 Base = declarative_base()
 
+# engine = create_engine('postgresql://andy:@andy@/ram')
+engine = create_engine('sqlite:///mbrDetials.db')
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine) 
+
+# Create a sessionmaker bound to the engine
+Session = sessionmaker(bind=engine)
 
 class MemberDetails(Base):
     """ A declarative class for the fooditem table """
@@ -23,40 +33,35 @@ class MemberDetails(Base):
     PHONE = Column(Integer)
     EMAIL = Column(String)
     
-    def __repr__(self):
-        return "<MemberDetails(MEMBER_ID='%s', FIRST_NAME='%s', LAST_NAME='%s', DOB='%s', ADDRESS='%s', CITY='%s', PHONE='%s', EMAIL='%s')>" % (
-                                self.MEMBER_ID, self.FIRST_NAME, self.LAST_NAME, self.DOB, self.ADDRESS, self.CITY, self.PHONE, self.EMAIL)
 
-# engine = create_engine('postgresql://andy:@andy@/ram')
-engine = create_engine('sqlite:///mbrDetials.db')
 
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine) 
+# Getting the data from Transformer which are validated and Adding to the session instant.
+context = zmq.Context()
+receiver = context.socket(zmq.PULL)
+receiver.bind("tcp://127.0.0.1:5558")
+load_data = receiver.recv()
+row_data = json.loads(load_data)
 
-# Create a sessionmaker bound to the engine
-Session = sessionmaker(bind=engine)
+# Date Formatting
+# DOB = datetime.datetime.strptime(row_data['DOB'], '%d/%m/%Y').date()
+
+# Add an instance of fooditem
+table_det1 = MemberDetails(MEMBER_ID=row_data['MEMBER_ID'], 
+                          FIRST_NAME=row_data['FIRST_NAME'],
+                          LAST_NAME=row_data['LAST_NAME'],
+                          DOB=row_data['DOB'], 
+                          ADDRESS=row_data['ADDRESS'], 
+                          CITY=row_data['CITY'],
+                          PHONE=row_data['PHONE'],
+                          EMAIL=row_data['EMAIL'])
+
 
 # Create a session
 session = Session()
 # help(session)
 
-# Date Formatting
-somestring = '3/1/2008'
-dob = datetime.datetime.strptime(somestring, '%d/%m/%Y').date()
-
-
-# Add an instance of fooditem
-table_det1 = MemberDetails(MEMBER_ID=9, FIRST_NAME='Anand', LAST_NAME='VS', \
-                          DOB=dob, ADDRESS='Basavangudi', CITY='Bangalore', \
-                          PHONE=1231231231, EMAIL='mail@mail.com')
-
-table_det2 = MemberDetails(MEMBER_ID=10, FIRST_NAME='Andy', LAST_NAME='VS', \
-                          DOB=dob, ADDRESS='Basavangudi', CITY='Bangalore', \
-                          PHONE=1231231231, EMAIL='mail@mail.com')
-
 # Adding to session
 session.add(table_det1)
-session.add(table_det2)
 
 # # Make some queries
 print session.query(MemberDetails).all()
